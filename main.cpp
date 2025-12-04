6bde59f442115ec1d0180c867436ee21c1e475e3
Hassan Sherif <hassan.sherif.da@gmail.com>
2025-12-04 18:11:43 +0200

    Initial Repo Push
    Test File for initial Repo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/**
+ * @file main.cpp
+ * @brief Main entry point for the programming project
+ * @author [Your Name]
+ * @date [Current Date]
+ */
+
+int main() {
+    cout << "Hello, World!" << endl;
+    
+    // TODO: Add your project code here
+    
+    return 0;
+}
+
